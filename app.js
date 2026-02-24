fb25926258bca48e36b34488994685a4d7ec149b
JPJuarz <jpjuarez@gmail.com>
2026-02-24 10:13:05 -0600

    Servidor con express

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
+
 const http = require('http');
 
 const html_header = `
