85bef82a09cd480070f4658fcf68e59f2184ab23
JPJuarz <jpjuarez@gmail.com>
2026-02-26 10:55:08 -0600

    lab 12 full

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+
 
 const rutasVideojuegos = require('./routes/videojuegos.routes')
 app.use('/videojuegos', rutasVideojuegos);
